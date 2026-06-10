e0ba74522d358c3d0c6f109374803707c737c0a6
Francisco Marquez Esteiro <marquez.esteiro.francisco@gmail.com>
2026-06-10 14:44:08 +0200

    Clean Architecture - Controlador y metodos

---
+++ CleanArchitectureTemplate.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,92 @@
+﻿using CleanArchitectureTemplate.Application.DTOs;
+using CleanArchitectureTemplate.Application.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureTemplate.Presentation.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var productos = await _productService.ObtenerProductosAsync();
+
+            return View(productos);
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear (ProductDTO productDTO)
+        {
+            if (!ModelState.IsValid)
+                return View(productDTO);
+
+            await _productService.AgregarProductoAsync(productDTO);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar (int id)
+        {
+            var product = await _productService.ObtenerProductoPorIdAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(ProductDTO productDTO)
+        {
+            if (!ModelState.IsValid)
+                return View(productDTO);
+
+            await _productService.ActualizarProductoAsync(productDTO);
+
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Eliminar (int id)
+        {
+            var producto = await _productService.ObtenerProductoPorIdAsync(id);
+
+            if (producto == null)
+                return NotFound();
+
+            return View(producto);
+        }
+
+        [HttpPost, ActionName("Eliminar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ConfirmarEliminar(int id)
+        {
+            var producto = await _productService.ObtenerProductoPorIdAsync(id);
+
+            if (producto == null)
+                return NotFound();
+
+            await _productService.EliminarProductoAsync(id);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
