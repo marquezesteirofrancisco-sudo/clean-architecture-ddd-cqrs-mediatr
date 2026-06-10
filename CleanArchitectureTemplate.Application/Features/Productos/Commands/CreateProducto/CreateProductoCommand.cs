34784b3db09d437d8727c7d44ed4ce5eaea37061
Francisco Marquez Esteiro <marquez.esteiro.francisco@gmail.com>
2026-06-10 21:10:43 +0200

    Clean Architecture(DDD, CQRS, MediatR) - CQRS y MediatR Crear Producto

---
+++ CleanArchitectureTemplate.Application/Features/Productos/Commands/CreateProducto/CreateProductoCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureTemplate.Application.Features.Productos.Commands.CreateProducto
+{
+    public class CreateProductoCommand : IRequest<int>
+    {
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
---
+++ CleanArchitectureTemplate.Application/Features/Productos/Commands/CreateProducto/CreateProductoHandler.cs
@@ -0,0 +1,41 @@
+﻿using CleanArchitectureTemplate.Application.DTOs;
+using CleanArchitectureTemplate.Domain.Entities;
+using CleanArchitectureTemplate.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureTemplate.Application.Features.Productos.Commands.CreateProducto
+{
+    public class CreateProductoHandler : IRequestHandler<CreateProductoCommand, int>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public CreateProductoHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<int> Handle(CreateProductoCommand request, CancellationToken cancellationToken)
+        {
+            // Implementacion del manejador para crear un producto
+            // Aqui iria la logica para guardar el producot en la base de datos
+            // Retornar el ID del producto creado
+
+
+            var producto = new Product(request.Nombre, request.Precio)
+            {
+                Descripcion = request.Descripcion
+            };
+
+
+            await _productRepository.AddAsync(producto);
+
+            return producto.Id;
+
+        }
+    }
+}
