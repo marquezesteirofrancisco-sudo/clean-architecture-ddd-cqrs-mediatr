43acd16c1280ceca12478740d049ee6da563e0e3
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-06-10 10:31:18 +0200

    Clean Architecture - DTOs (Data Transfer Object)
    Clean Architecture - DTOs (Data Transfer Object)

---
+++ CleanArchitectureTemplate.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureTemplate.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Debe introducir el nombre del producto obligatoriamente.")]
+        [StringLength (100)]
+        public string Nombre { get; set; }
+
+        [Range (0, 100)]
+        public decimal Precio { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
