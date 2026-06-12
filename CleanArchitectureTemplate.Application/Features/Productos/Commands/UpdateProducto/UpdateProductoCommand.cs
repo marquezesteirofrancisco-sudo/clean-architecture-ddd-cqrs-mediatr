17bb00dec983a3a30b11931697a22189781ab4fb
Francisco Marquez Esteiro <marquez.esteiro.francisco@gmail.com>
2026-06-12 10:12:04 +0200

    Clean Architecture(DDD, CQRS, MediatR) - CQRS y MediatR  Actualizar Producto

---
+++ CleanArchitectureTemplate.Application/Features/Productos/Commands/UpdateProducto/UpdateProductoCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+
+namespace CleanArchitectureTemplate.Application.Features.Productos.Commands.UpdateProducto
+{
+    public class UpdateProductoCommand : IRequest
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
---
+++ CleanArchitectureTemplate.Application/Features/Productos/Commands/UpdateProducto/UpdateProductoHandler.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchitectureTemplate.Domain.Interfaces;
+using MediatR;
+
+
+namespace CleanArchitectureTemplate.Application.Features.Productos.Commands.UpdateProducto
+{
+    public class UpdateProductoHandler : IRequestHandler<UpdateProductoCommand>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public UpdateProductoHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateProductoCommand request, CancellationToken cancellationToken)
+        {
+            // buscamos el producto por el id de la request
+            var producto = await _productRepository.GetByIdAsync(request.Id);
+
+            // si no lo encontramos lanzamos una excepticion
+            if (producto == null)
+                throw new Exception("Producto no encontrado");
+
+            // actualizamos las propiedades del produccto
+            producto.Nombre = request.Nombre;
+            producto.Descripcion = request.Descripcion;
+            producto.Precio = request.Precio;
+
+            // llamamos al mentodo para acualizar en la base de datos
+            await _productRepository.UpdateAsync(producto);
+
+            return Unit.Value;
+        }
+    }
+}
